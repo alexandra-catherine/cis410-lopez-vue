89177c19b752c25225c44ca2ffb9fd459031f483
alexandra-catherine <acl21@colostate.edu>
2020-12-02 12:28:49 -0700

    15.2 12/2 details page

--- src/components/WorkoutTypeDetails.vue
+++ src/components/WorkoutTypeDetails.vue
@@ -1,12 +1,45 @@
 <template>
     <div>
-        <h1>Workout Type Details</h1>
+        <div class = "card ">
+            <div class = 'card-body'>
+                <h2 class = "text-danger">{{workoutType.WorkoutTypeName}}</h2>
+                <br/>
+                <p>Description: <br/> 
+                    <strong>{{workoutType.Description}}</strong>
+                </p>
+                <p>Difficuly Rate: <br/> 
+                    <strong>{{workoutType.Difficulty}}</strong>
+                </p>
+                 <p>Famous Athletes: <br/> 
+                    <strong>{{workoutType.Famous}}</strong>
+                </p>
+            </div>
+        </div>
+
+        <router-link v-if="auth" :to="`/workoutType/${this.$route.params.pk}/create`">
+            <button type="button" class="btn btn-danger">New Workout</button>
+        </router-link>
+
+        <router-link v-else :to="`/login`">
+            <button type="button" class="btn btn-outline-danger">Sign in to add this Workout</button>
+        </router-link>
+
     </div>
 </template>
 
 <script>
 export default {
-    
+    computed:{
+        workoutType(){
+            var workoutType = this.$store.state.workoutType;
+            var thisWorkoutType = workoutType.find((aType)=>aType.WorkoutTypeID == this.$route.params.pk)
+
+           // console.log("this is the type that you want", thisWorkoutType);
+            return thisWorkoutType
+        },
+
+        auth(){return this.$store.state.token}
+    }
 }
 </script>
 
